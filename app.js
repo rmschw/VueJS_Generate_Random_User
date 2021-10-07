9985876b8ed4bc2955158b524e6e4147d6d75976
rmschw <raam050193@gmail.com>
2021-10-07 13:50:38 -0400

    update changes

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const app = Vue.createApp({
             lastName: 'Doe',
             email: 'john@gmail.com',
             gender: 'male',
+            phone: 123456789,
+            age: 24,
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
     methods: {
-        getUser() {
-            console.log(this.firstName)
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+
+            console.log(results)
+
+
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.phone = results[0].phone,
+            this.age = results[0].dob.age,
+            this.picture = results[0].picture.large
         },
     },
 })
